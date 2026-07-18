83659ff646490f8c88733ce741b4b184dca24617
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-07-18 23:36:04 +0400

    feat: implement dark and light theme switcher functionality

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const btnMenu = document.querySelectorAll(`.btn--menu`);
 
 const appContainer = document.querySelector(`.app-container`);
 
+const body = document.querySelector(`body`);
+const themeSwitcher = document.querySelector(`.btn--theme-switcher`);
+
 closeOpenBtn.addEventListener(`click`, () => {
   btnMenu.forEach((btn) => btn.classList.toggle(`display-none`));
   appContainer.classList.toggle(`translate`);
+});
+
+themeSwitcher.addEventListener(`click`, () => {
+  body.classList.toggle(`body--dark`);
 });
